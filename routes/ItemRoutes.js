f712edbbd5d466479b2f76a9ceef3deb8e3ac83f
AnugrahPanjiPradipa <panjipradipa714@gmail.com>
2026-04-12 20:21:28 +0700

    geraiii

--- routes/ItemRoutes.js
+++ routes/ItemRoutes.js
@@ -7,9 +7,8 @@ const {
   penjualan,
 } = require("../controllers/itemController");
 const itemController = require("../controllers/itemController");
-const upload = require("../middleware/upload");
 
-router.post("/", upload.single("image"), itemController.createItem);
+
 router.get("/", getItems);
 router.put("/mutasi/:id", itemController.mutasiGudang);
 router.put("/penjualan/:id", itemController.penjualan);

5fe6ea39d8ceb1b0e4ffdca93827d6341b042717
AnugrahPanjiPradipa <panjipradipa714@gmail.com>
2026-04-12 20:35:41 +0700

    geraiiiii

--- routes/ItemRoutes.js
+++ routes/ItemRoutes.js
@@ -10,6 +10,7 @@ const itemController = require("../controllers/itemController");
 
 router.post("/", itemController.createItem);
 router.get("/", getItems);
+router.put("/:id", itemController.updateItem);
 router.put("/mutasi/:id", itemController.mutasiGudang);
 router.put("/penjualan/:id", itemController.penjualan);
 router.delete("/:id", itemController.deleteItem);

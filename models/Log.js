9433bd6c41181fe571e655a5368975df8788a882
AnugrahPanjiPradipa <panjipradipa714@gmail.com>
2026-04-13 20:53:19 +0700

    Log.js fix

--- models/Log.js
+++ models/Log.js
@@ -4,10 +4,9 @@ const logSchema = new mongoose.Schema(
   {
     itemId: { type: mongoose.Schema.Types.ObjectId, ref: "Item" },
     itemName: String,
-    type: { type: String, enum: ["input", "mutasi", "penjualan"] },
+    type: { type: String, enum: ["input", "mutasi", "penjualan", "transfer", "pengurangan"] },
     asal: String,
     tujuan: String,
-    jumlah: Number,
   },
   { timestamps: true },
 );
